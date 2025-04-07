c689fd19d3b8ac8126dc7f8ad5e52f8ec7ae4854
lizfly256 <lizpow256@gmail.com>
2025-04-07 15:53:53 -0500

    Create staff_into.py

---
+++ staff_into.py
@@ -0,0 +1,24 @@
+staff_list = [
+    
+    {
+        'name': 'Stacy',
+        'max hour': 32,
+        'min hour': 20,
+        'pref day': ['mon', 'tues', 'fri'],
+        'available days': ['mon', 'tues', 'wed', 'thrus', 'fri']
+    },
+    {
+        'name': 'Steve',
+        'max hour': 25,
+        'min hour': 20,
+        'pref day': ['mon', 'wed', 'fri'],
+        'available days': ['sun', 'mon', 'tues', 'wed', 'thurs', 'fri', 'sat']
+    },
+    {
+        'name': 'Amanda',
+        'max hour': 20,
+        'min hour': 30,
+        'pref day': ['sun', 'mon', 'tues', 'wed'],
+        'available days': ['sun', 'mon', 'tues', 'wed']
+    }
+]
