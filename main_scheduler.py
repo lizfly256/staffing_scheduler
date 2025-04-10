e043684ae87974efa7aa7684f1658657e82cb61c
lizfly256 <lizpow256@gmail.com>
2025-04-10 10:29:48 -0500

    Update main_scheduler.py

--- main_scheduler.py
+++ main_scheduler.py
@@ -1,32 +1,43 @@
-from staff_info import staff_list
+class Staff:
+    '''This creates staff and their scheduling related info'''
+    _registry = []
+    def __init__(self, name, pref, avail, hoursMin, hoursMax):
+        self._registry.append(self)
+        self.name = name
+        self.pref = pref
+        self.avail = avail
+        self.hours_min = hoursMin
+        self.hour_max = hoursMax
 
-def start_week(staff_list):
-    """Returns a dict with which to keep track of how many hours each staffer has been scheduled."""
-    hours_total = dict()
-    for staff in staff_list:
-        staff_name = staff["name"]
-        hours_total[staff_name] = 0
-    print(hours_total)
-    return hours_total
 
-def get_options(new_shift, hours_total):
-    """Returns best selection of staffers to fill shift"""
-    best_options = dict()
-    for staff in staff_list:
-        if new_shift in staff['pref day'] and under_min_hour(staff) is True:
-            print('ACCESSED') #add them to best_options
+hazel = Staff('Hazel', ['wed', 'thurs'], ['mon', 'tues', 'wed', 'thurs'], 20, 30)
+willa = Staff('Willa', ['sat', 'sun'], ['fri', 'sat', 'sun'], 10, 20)
+homer = Staff('Homer', ['mon', 'wed', 'fri'], ['sun', 'mon', 'tues', 'wed', 'thurs', 'fri', 'sat'], 20, 30)
+ollie = Staff('Ollie', ['mon', 'tues', 'wed', 'thurs'], ['mon', 'tues', 'wed', 'thurs', 'fri'], 25, 35)
+juliet = Staff('Juliet', ['sat', 'sun'],['sat', 'mon', 'fri', 'sun'], 20, 30)
+ruby = Staff('Ruby', ['tues', 'thurs'], ['sun', 'mon', 'tues', 'wed', 'thurs', 'fri', 'sat'], 10, 30)
+maya = Staff('Maya', ['thurs', 'fri'], ['wed', 'thurs', 'fri', 'sat'], 10, 35)
+teddy = Staff('Teddy', ['wed', 'thurs'],['tues', 'wed', 'thurs'], 10, 30)
+bear = Staff('Bear', ['sat', 'sun'], ['sat', 'wed', 'sun'], 10, 20)
+bob = Staff('Bob', ['mon'], ['mon', 'tues'], 25, 35)
+kima = Staff('Kima', ['tues'], ['sun', 'mon', 'tues', 'wed', 'thurs', 'fri', 'sat'], 15, 25)
+billie = Staff('Billie', [],['mon'], 0, 10)
+
+
+#who prefers a day and needs more hour
+schedule = dict()
+new_shift = input("day: ")
+prefers_shift = list()
+for s in Staff._registry:
+    if new_shift in s.pref:
+        prefers_shift.append(s.name)
     
-    # preffers day and hasn't met minimum
-    # available and hasn't met minimum
-    # prefers day and isn't over max
-    # available and isn't over max
-        # add shift to schedule with selected staffer, update staffs running total
-    
-hours_total = start_week(staff_list)
-new_shift = input('Shift day: ')
-get_options(new_shift, hours_total)
+print(prefers_shift)
+assigned_staffer = input("Select staffer for " + str(new_shift)).lower()
+schedule[new_shift] = assigned_staffer  #dictionary[key] = value
 
 
+print(schedule)
 
 
 
